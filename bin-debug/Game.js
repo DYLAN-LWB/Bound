a009e14ca177518945b7dbc543ee7e641b97ae98
李伟宾 <dylan_lwb@163.com>
2017-07-28 11:06:59 +0800

    添加背景云彩

--- bin-debug/Game.js
+++ bin-debug/Game.js
@@ -37,12 +37,8 @@ var Game = (function (_super) {
     Game.prototype.onAddToStage = function (event) {
         this.stageW = this.stage.stageWidth;
         this.stageH = this.stage.stageHeight;
-        //舞台背景
-        var background = new egret.Sprite;
-        background.graphics.beginFill(0x7AC5CD, 1);
-        background.graphics.drawRect(0, 0, this.stageW, this.stageH);
-        background.graphics.endFill();
-        this.addChild(background);
+        //背景云彩
+        this.addCloud();
         //添加台阶, 台阶添加背景容器来控制x值
         for (var i = 0; i < this.totalStep; i++) {
             var step = this.createBitmapByName("ladder_png");
@@ -234,6 +230,43 @@ var Game = (function (_super) {
     //加速
     Game.prototype.speedUp = function () {
         // this.observeHit(true);
+    };
+    //添加背景
+    Game.prototype.addCloud = function () {
+        //舞台背景
+        var background = new egret.Sprite;
+        background.graphics.beginFill(0x7AC5CD, 1);
+        background.graphics.drawRect(0, 0, this.stageW, this.stageH);
+        background.graphics.endFill();
+        this.addChild(background);
+        var cloud1 = this.createBitmapByName("yun01_png");
+        cloud1.x = 20;
+        cloud1.y = 111;
+        cloud1.width = 97;
+        cloud1.height = 70;
+        this.addChild(cloud1);
+        egret.Tween.get(cloud1, { loop: true }).to({ x: this.stageW }, 35000).to({ x: -100 }, 35000);
+        var cloud2 = this.createBitmapByName("yun03_png");
+        cloud2.x = 200;
+        cloud2.y = 211;
+        cloud2.width = 144;
+        cloud2.height = 82;
+        this.addChild(cloud2);
+        egret.Tween.get(cloud2, { loop: true }).to({ x: this.stageW }, 50000).to({ x: -200 }, 50000);
+        var cloud3 = this.createBitmapByName("yun02_png");
+        cloud3.x = 640;
+        cloud3.y = 500;
+        cloud3.width = 73;
+        cloud3.height = 44;
+        this.addChild(cloud3);
+        egret.Tween.get(cloud3, { loop: true }).to({ x: -100 }, 30000).to({ x: this.stageW }, 30000);
+        var cloud4 = this.createBitmapByName("yun04_png");
+        cloud4.x = 40;
+        cloud4.y = 644;
+        cloud4.width = 112;
+        cloud4.height = 76;
+        this.addChild(cloud4);
+        egret.Tween.get(cloud4, { loop: true }).to({ x: this.stageW }, 40000).to({ x: -120 }, 40000);
     };
     return Game;
 }(egret.DisplayObjectContainer));
--- src/Game.ts
+++ src/Game.ts
@@ -52,12 +52,11 @@ class Game extends egret.DisplayObjectContainer {
 		this.stageW = this.stage.stageWidth;
 		this.stageH = this.stage.stageHeight;
 		
-		//舞台背景
-		let background = new egret.Sprite;
-        background.graphics.beginFill(0x7AC5CD,1);
-        background.graphics.drawRect(0,0,this.stageW,this.stageH);
-        background.graphics.endFill();
-        this.addChild(background);
+
+
+
+		//背景云彩
+        this.addCloud();
 
 		//添加台阶, 台阶添加背景容器来控制x值
 		for(var i = 0; i < this.totalStep; i++) {
@@ -101,6 +100,7 @@ class Game extends egret.DisplayObjectContainer {
         this.metersLabel.size = 30;
         this.metersLabel.text = "您已经走了" + this.metersCount + "米";
         this.addChild(this.metersLabel);
+
     }
 
 	private addTouchEvent() {
@@ -251,7 +251,6 @@ class Game extends egret.DisplayObjectContainer {
 				// 	index = 6;
 				// }
 				this.metersCount += Math.round(moveLen/100);
-
 				this.metersLabel.text = "您已经走了" + this.metersCount + "米";
 
 				//遍历数组 改变x值
@@ -306,6 +305,52 @@ class Game extends egret.DisplayObjectContainer {
 		// this.observeHit(true);
 
 	}
+
+
+
+
+	 //添加背景
+    private addCloud() {
+        //舞台背景
+		let background = new egret.Sprite;
+        background.graphics.beginFill(0x7AC5CD,1);
+        background.graphics.drawRect(0,0,this.stageW,this.stageH);
+        background.graphics.endFill();
+        this.addChild(background);
+
+        let cloud1 = this.createBitmapByName("yun01_png");
+        cloud1.x = 20;
+        cloud1.y = 111;
+        cloud1.width = 97;
+        cloud1.height = 70;
+        this.addChild(cloud1);
+        egret.Tween.get(cloud1, {loop:true}).to({x:this.stageW}, 35000).to({x:-100}, 35000);
+
+        let cloud2 = this.createBitmapByName("yun03_png");
+        cloud2.x = 200;
+        cloud2.y = 211;
+        cloud2.width = 144;
+        cloud2.height = 82;
+        this.addChild(cloud2);
+        egret.Tween.get(cloud2, {loop:true}).to({x:this.stageW}, 50000).to({x:-200}, 50000);
+
+        let cloud3 = this.createBitmapByName("yun02_png");
+        cloud3.x = 640;
+        cloud3.y = 500;
+        cloud3.width = 73;
+        cloud3.height = 44;
+        this.addChild(cloud3);
+        egret.Tween.get(cloud3, {loop:true}).to({x:-100}, 30000).to({x:this.stageW}, 30000);
+
+        let cloud4 = this.createBitmapByName("yun04_png");
+        cloud4.x = 40;
+        cloud4.y = 644;
+        cloud4.width = 112;
+        cloud4.height = 76;
+        this.addChild(cloud4);
+        egret.Tween.get(cloud4, {loop:true}).to({x:this.stageW}, 40000).to({x:-120}, 40000);
+
+    }
 
 	
 
